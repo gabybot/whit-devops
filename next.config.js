cc0c5faa7acfd9723bc60b8617b6c08dc5f7ea6e
Gabriela Andrea Moya <123262079+gabybot@users.noreply.github.com>
2024-08-29 17:17:46 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    output: 'standalone',
-  }
+  output: 'standalone',
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,  // Añade esta línea
+  },
+};
